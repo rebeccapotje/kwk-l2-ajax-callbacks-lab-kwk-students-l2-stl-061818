491ad8542c7f84abef429403329cbca54f2cc681
rebeccapotje <rebecca.potje@gmail.com>
2018-06-26 15:54:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
+
+
+//$.ajax({
+  //url: 'https://api.github.com/search/repositories',
+  //dataType: 'json',
+  //data: {
+    //api_key: 
+    //q: $('#searchTerms').val(), //input,
+    
+  //}
+  
+//}
+  //)
+
+
 $(document).ready(function (){
 });
+
+function searchRepositories(){
+ const searchTerms = $( "#searchTerms").val();
+ $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+   $(`#results`).html(data);
+ })
+
+}
+
+var renderSearchResults = (data) => data.items.map(result => renderSearchResults(result))
+
+var renderSearchResults = (result) => {
+ return `
+   <div>
+   
+   </div>'
+}
+
